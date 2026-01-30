e40fa01de4f1d30aae5cd6da89392765d9083dbb
Aakarsh <host.fast20@gmail.com>
2026-01-30 18:19:51 +0530

    day 1
    Import all libraries in python

---
+++ Importing_Libraries/libraries.py
@@ -0,0 +1,11 @@
+# Step 1 : - importing all required Libraries 
+
+# We are using OpenCV,EasyOCR,matplotlib,numpy,random and imutils
+
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import easyocr
+import imutils
+import random
+
